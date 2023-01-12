dd8b8a3c7d9e584fb90e5741d7d6983ec566ffd9
Andrew <Asaganda@gmail.com>
2023-01-12 12:27:44 -0500

    created separate component for each coord record that will populate list, data flowing nicely

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,28 +1,14 @@
 // import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import EachCoord from '../component/EachCoord.js'
 
 const List = (props) => {
-    // const [coords, setCoords] = useState([...props.coords])
-
-    const deleteCoord = (coord) => {
-        console.log('delete coord')
-        console.log(coord)
-        props.handleDelete(coord)
-    }
+    
     return(
         <>
             <Link to="/"><button>Back to Home</button></Link>
             <h2>List of records</h2>
-            {props.coords.map(coord => {
-                return(
-                <div key={coord._id}>
-                    <p>ID: {coord._id}</p>
-                    <p>latitude: {coord.lat}, longitude: {coord.lng}</p>
-                    <button onClick={() => deleteCoord(coord)}>Delete</button>
-                    <button>Edit</button>
-                </div>
-                )
-            })}
+            <EachCoord coords={props.coords} handleDelete={props.handleDelete}/>
         </>
     )
 }
