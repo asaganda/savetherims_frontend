d2192c614b8feaea3fddacb45a2f1fcc27058b28
Andrew <Asaganda@gmail.com>
2023-01-18 21:57:51 -0500

    added status messages on home page when recording location

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 const Home = (props) => {
     return(
         <>
-            {/* <h1>SaveTheRims</h1> */}
+            {props.submitStatus ? <h3>Recording location..</h3>: null}
+            {props.recordSuccess? <h3>Success</h3> : null}
             <button onClick={() => props.handleGeolocate()}>Submit</button>
             <Link to="/list"><button>Go to List page</button></Link>
             <Link to="/map"><button>Go to Map page</button></Link>
