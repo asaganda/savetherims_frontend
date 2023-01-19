2327ad1a15bf51e7fde5464e95ad87f42e19a68c
Andrew <Asaganda@gmail.com>
2023-01-18 22:02:05 -0500

    refactored success logic to short circuit to try it out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 const Home = (props) => {
     return(
         <>
-            {props.submitStatus ? <h3>Recording location..</h3>: null}
-            {props.recordSuccess? <h3>Success</h3> : null}
+            {props.submitStatus && <h3>Recording location..</h3>}
+            {props.recordSuccess && <h3>Success</h3>}
             <button onClick={() => props.handleGeolocate()}>Submit</button>
             <Link to="/list"><button>Go to List page</button></Link>
             <Link to="/map"><button>Go to Map page</button></Link>
