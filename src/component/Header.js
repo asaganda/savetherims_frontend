876ed172862f4145ae6635f414f2249912150078
Andrew <Asaganda@gmail.com>
2023-03-06 22:01:54 -0500

    added heroicons package for info icon to main page next to title

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import TailwindHeaderBrand from "./TailwindHeaderBrand"
+import { InformationCircleIcon } from "@heroicons/react/24/outline"
 
 const Header = () => {
     return(
         <>
             <TailwindHeaderBrand>
                 SaveTheRims
+                <InformationCircleIcon className="h-6 w-6"/>
             </TailwindHeaderBrand>
             <Outlet/>
         </>
