9304f06fd4d1ea97f9e129cd97c83062cacf5818
Andrew <Asaganda@gmail.com>
2023-06-08 22:24:50 -0400

    comment out last test in Home test

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -15,3 +15,15 @@ it('renders the submit button', () => {
     const submitBtn = screen.getByText('Submit')
     expect(submitBtn).toBeInTheDocument();
 });
+
+// it('clicking submit to record new pothole', () => {
+//     const mockedHandleGeolocate = jest.fn()
+//     render(<MockHome />)
+//     const submitBtn = screen.getByText('Submit')
+//     fireEvent.click(submitBtn);
+//     expect(mockedHandleGeolocate).toHaveBeenCalled()
+//     // handleGeolocate function invoked - we should mock this
+//         // geolocate function gets invoked which inside has a few functions inside
+//     // expect to get mock current position
+//     // expect to create a new mock record
+// })
