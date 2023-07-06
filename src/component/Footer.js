b9b3c9bbd48c221ff3d10749c92ae17d6362b981
Andrew <Asaganda@gmail.com>
2023-07-05 21:44:00 -0400

    refactor footer alignment

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <div className="bg-brand_orange w-full bottom-0 absolute flex flex-col content-center text-sm p-2">
-            <p>Powered with MERN, Made by Andrew</p>
+        <div className="bg-brand_orange w-full bottom-0 absolute flex justify-center text-sm p-2">
+            <p>Powered with MERN, <a href="https://github.com/asaganda/savetherims_frontend">Made by Andrew</a></p>
         </div>
     )
 }
